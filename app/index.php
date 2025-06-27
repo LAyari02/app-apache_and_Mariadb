448ccc2ad9233eb23d919ddadf1047ebe8b8c9e3
LAyari02 <saifayariabidi@gmail.com>
2025-06-27 19:48:27 +0200

    changement pour faire le test avec la base de donnée.

--- app/index.php
+++ app/index.php
@@ -1,3 +1,8 @@
 <?php
-echo "Hello World!";
+$mysqli = new mysqli("db", "user", "password", "testdb");
+if ($mysqli->connect_errno) {
+    echo "Échec de connexion à MariaDB: " . $mysqli->connect_error;
+} else {
+    echo "Connexion réussie à MariaDB!";
+}
 ?>
